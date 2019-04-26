bb7b20a65f018afe235c15d40271b7c00c6e6d85
Naman Gulati <40243817+naman-gulati3@users.noreply.github.com>
2019-04-26 12:05:05 +0530

    Delete omniauth.rb

--- prac/config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-opts = { scope: 'user:email' }
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret, opts
-  provider :facebook, '598054710707195', 'a3e1339322c8401cc78c791e6ccaa2e8'
-end
